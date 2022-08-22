64e1f06c9584a1071cffc583044c5def8a1a64a9
Louis Michaud <michaud.louis@yahoo.fr>
2022-08-22 18:06:35 +0200

    Addition of routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,11 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: "pages#home"
+  resources :shows do
+    resources :bookings, only: [:new, :create]
+  end
+  resources :bookings, only: [:index, :update, :destroy]
+  get "dashboard", to: "pages#dashboard"
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
