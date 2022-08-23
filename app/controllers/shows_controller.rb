6b9740dbcec61a60b524dc43cd571c9736db5860
Yann Deret <yann.deretleberre@gmail.com>
2022-08-23 11:41:29 +0200

    basic edit & update

--- app/controllers/shows_controller.rb
+++ app/controllers/shows_controller.rb
@@ -12,11 +12,26 @@ class ShowsController < ApplicationController
   end
 
   def edit
+    @show = Show.find(params[:id])
   end
 
   def update
+    @show = Show.find(params[:id])
+    @show.update(task_params)
+    if @show.save
+      redirect_to show_path(@show)
+    else
+      render :new, status: :unprocessable_entity
+    end
   end
 
   def destroy
   end
+
+  private
+
+  def show_params
+    params.require(:show).permit(:name, :category_id, :description, :company, :location_center, :location_radius, :price)
+  end
+
 end
