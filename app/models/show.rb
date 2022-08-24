bd745fea2a597f2fedeb161a06aaccfb61c0df43
Louis Michaud <michaud.louis@yahoo.fr>
2022-08-24 16:51:22 +0200

    Debug destroy + signup + seed

--- app/models/show.rb
+++ app/models/show.rb
@@ -1,7 +1,7 @@
 class Show < ApplicationRecord
   belongs_to :category
   belongs_to :user
-  has_many :bookings
+  has_many :bookings, dependent: :destroy
   has_many :users, through: :bookings
   has_many_attached :photos
   validates :name, :description, :company, :location_center, :location_radius, :price, presence: true
--- app/models/user.rb
+++ app/models/user.rb
@@ -3,8 +3,8 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
-  has_many :shows
-  has_many :bookings
+  has_many :shows, dependent: :destroy
+  has_many :bookings, dependent: :destroy
 
   validates :name, presence: true
 end
